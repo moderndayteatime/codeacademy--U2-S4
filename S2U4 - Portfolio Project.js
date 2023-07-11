2fc6acfccf4b477d1a92a1a639f79fc5a92f2f35
Amir <moderndayteatime@gmail.com>
2023-07-11 16:50:43 -0500

    Added some comments

--- S2U4 - Portfolio Project.js
+++ S2U4 - Portfolio Project.js
@@ -8,9 +8,10 @@ const getRandomNum = num => {
 let storage = {
     name : ["Brent", "Claire", "Charlie", "Kevin", "Sideways", "Butch", "Cereal-killer", "Nelly"],
     type: ["Assault", "Medic", "Engineer", "Support", "Recon", "Scout", "Commander"],
-    slogan: ["destroy", "love", "suprise", "ambush", "take care of", "anihilate", "kill", "eat", "butcher", "have tea time", "duel", "dance with", "cuddle", " get killed by"], //I'm gonna () the enemy
+    slogan: ["destroy", "love", "suprise", "ambush", "take care of", "anihilate", "kill", "eat", "butcher", "have tea time with", "duel", "dance with", "cuddle", "get killed by"], //I'm gonna () the enemy
 };
 
+//Gets character information and outputs it as a prompt
 const getCharacter = (name, type, slogan) => {
     
     console.log(`My name is ${name}.`);
@@ -18,9 +19,11 @@ const getCharacter = (name, type, slogan) => {
     console.log(`I'm going to ${slogan} the enemy!`);
 }
 
+//Gets the parameter for getCharacter using storage object and getRandomNum to find the index.
 let getName = storage.name[getRandomNum(storage.name.length)];
 let getType = storage.type[getRandomNum(storage.type.length)];
 let getSlogan = storage.slogan[getRandomNum(storage.slogan.length)]
 
+//Get your character!
 getCharacter(getName, getType, getSlogan);
 
